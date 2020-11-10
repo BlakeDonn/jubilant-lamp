1a92f1b3343989eca36cfc380a89b751ae887b7b
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-10 11:50:02 -0500

    Add slight error handling for post

--- src/components/UrlForm/UrlForm.js
+++ src/components/UrlForm/UrlForm.js
@@ -1,57 +1,61 @@
-import React, { Component } from 'react';
-import {postUrls} from '../../apiCalls'
+import React, { Component } from "react";
+import { postUrls } from "../../apiCalls";
 
 class UrlForm extends Component {
   constructor(props) {
     super();
     this.props = props;
     this.state = {
-      title: '',
-      urlToShorten: ''
+      title: "",
+      urlToShorten: "",
     };
   }
 
-  handleNameChange = e => {
+  handleNameChange = (e) => {
     this.setState({ [e.target.name]: e.target.value });
-  }
+  };
 
-  handleSubmit = e => {
+  handleSubmit = async (e) => {
     e.preventDefault();
-    postUrls({
+    if (!this.state.title || !this.state.urlToShorten){
+    this.clearInputs();
+      return alert("please fill out title and url")
+    }
+    const response = await postUrls({
       long_url: this.state.urlToShorten,
       title: this.state.title,
-    })
+    });
+    console.log(response)
+
     this.clearInputs();
-  }
+  };
 
   clearInputs = () => {
-    this.setState({title: '', urlToShorten: ''});
-  }
+    this.setState({ title: "", urlToShorten: "" });
+  };
 
   render() {
     return (
       <form>
         <input
-          type='text'
-          placeholder='Title...'
-          name='title'
+          type="text"
+          placeholder="Title..."
+          name="title"
           value={this.state.title}
-          onChange={e => this.handleNameChange(e)}
+          onChange={(e) => this.handleNameChange(e)}
         />
 
         <input
-          type='text'
-          placeholder='URL to Shorten...'
-          name='urlToShorten'
+          type="text"
+          placeholder="URL to Shorten..."
+          name="urlToShorten"
           value={this.state.urlToShorten}
-          onChange={e => this.handleNameChange(e)}
+          onChange={(e) => this.handleNameChange(e)}
         />
 
-        <button onClick={e => this.handleSubmit(e)}>
-          Shorten Please!
-        </button>
+        <button onClick={(e) => this.handleSubmit(e)}>Shorten Please!</button>
       </form>
-    )
+    );
   }
 }
 
