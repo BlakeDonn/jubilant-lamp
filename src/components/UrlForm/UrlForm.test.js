fd6fe0105ee570380c6e66cb8709c7bb05e95e14
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-10 12:29:26 -0500

    Add unit test for user typing input into form

--- src/components/UrlForm/UrlForm.test.js
+++ src/components/UrlForm/UrlForm.test.js
@@ -1,5 +1,6 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import React from 'react'
+import userEvent from "@testing-library/user-event";
 import "@testing-library/jest-dom";
 import  UrlForm  from "./UrlForm";
 
@@ -10,6 +11,14 @@ describe("UrlContainer", () =>{
     expect(screen.getByPlaceholderText("Title...")).toBeInTheDocument()
     expect(screen.getByPlaceholderText("URL to Shorten...")).toBeInTheDocument()
     expect(screen.getByRole("button",{name: "Shorten Please!"})).toBeInTheDocument()
+  })
+
+  it ("Should capture user inputs when typing in form", () =>{
+    render(<UrlForm />)
+    userEvent.type(screen.getByPlaceholderText("Title..."), "TestTitle");
+    userEvent.type(screen.getByPlaceholderText("URL to Shorten..."), "TestURL");
+    expect(screen.getByPlaceholderText("Title...")).toHaveValue("TestTitle")
+    expect(screen.getByPlaceholderText("URL to Shorten...")).toHaveValue("TestURL")
   })
 
 })
