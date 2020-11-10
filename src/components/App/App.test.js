9e08878389ceace1d102761c5a1594007d355eed
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-10 13:48:45 -0500

    Remove logs

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,7 +2,7 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import "@testing-library/jest-dom";
-import { getUrls, postUrls} from "../../apiCalls.js";
+import { getUrls, postUrls, deleteUrl } from "../../apiCalls.js";
 import { App } from "./App";
 jest.mock("../../apiCalls.js");
 
@@ -56,9 +56,45 @@ describe("App", () => {
     userEvent.type(screen.getByPlaceholderText("Title..."), "TestTitle");
     userEvent.type(screen.getByPlaceholderText("URL to Shorten..."), "TestURL");
     userEvent.click(screen.getByRole("button", { name: "Shorten Please!" }));
-    await waitFor(() => expect(screen.getByText("userTestTitle")).toBeInTheDocument());
+    await waitFor(() =>
+      expect(screen.getByText("userTestTitle")).toBeInTheDocument()
+    );
     expect(screen.getByText("userTestTitle")).toBeInTheDocument();
-    expect(screen.getByText("http://localhost:3001/useshorturl/14")).toBeInTheDocument();
+    expect(
+      screen.getByText("http://localhost:3001/useshorturl/14")
+    ).toBeInTheDocument();
   });
 
+  it("User should be able to delete a URL and have the dom update accordingly", async () => {
+    deleteUrl.mockResolvedValue({
+      ok: true,
+      redirected: false,
+      status: 204,
+      statusText: "No Content",
+      type: "cors",
+      url: "testURl",
+    });
+    render(<App />);
+    await waitFor(() => expect(screen.getByText("title1")).toBeInTheDocument());
+    getUrls.mockResolvedValue({
+      urls: [
+        {
+          id: 2,
+          long_url: "longurl2",
+          short_url: "shorturl2",
+          title: "title2",
+        },
+        {
+          id: 3,
+          long_url: "longurl3",
+          short_url: "shorturl3",
+          title: "title3",
+        },
+      ],
+    });
+    userEvent.click(screen.getByTestId("title1-button", { name: "Delete me" }));
+    await waitFor(
+      () => !expect(screen.getByText("title1")).toBeInTheDocument()
+    );
+  });
 });
