f9552ca07ccda607e33ba4c87852f5ff07e7c478
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-10 13:08:41 -0500

    Add final integration test for user adding url to DOm

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,7 +2,7 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import "@testing-library/jest-dom";
-import { getUrls } from "../../apiCalls.js";
+import { getUrls, postUrls} from "../../apiCalls.js";
 import { App } from "./App";
 jest.mock("../../apiCalls.js");
 
@@ -43,4 +43,22 @@ describe("App", () => {
     expect(screen.getByText("title2")).toBeInTheDocument();
     expect(screen.getByText("title3")).toBeInTheDocument();
   });
+
+  it("User should be able to add new url to server and DOM", async () => {
+    postUrls.mockResolvedValue({
+      id: 14,
+      long_url: "testURL",
+      short_url: "http://localhost:3001/useshorturl/14",
+      title: "userTestTitle",
+    });
+    render(<App />);
+    await waitFor(() => expect(screen.getByText("title1")).toBeInTheDocument());
+    userEvent.type(screen.getByPlaceholderText("Title..."), "TestTitle");
+    userEvent.type(screen.getByPlaceholderText("URL to Shorten..."), "TestURL");
+    userEvent.click(screen.getByRole("button", { name: "Shorten Please!" }));
+    await waitFor(() => expect(screen.getByText("userTestTitle")).toBeInTheDocument());
+    expect(screen.getByText("userTestTitle")).toBeInTheDocument();
+    expect(screen.getByText("http://localhost:3001/useshorturl/14")).toBeInTheDocument();
+  });
+
 });
