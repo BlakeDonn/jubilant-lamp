3f9e51e97950265919a38e5e1ff80a5133c65174
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-10 13:25:17 -0500

    Add delete functionality and pass down to urlContainer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,35 +1,43 @@
-import React, { Component } from 'react';
-import './App.css';
-import { getUrls } from '../../apiCalls';
-import UrlContainer from '../UrlContainer/UrlContainer';
-import UrlForm from '../UrlForm/UrlForm';
+import React, { Component } from "react";
+import "./App.css";
+import { getUrls, deleteUrl } from "../../apiCalls";
+import UrlContainer from "../UrlContainer/UrlContainer";
+import UrlForm from "../UrlForm/UrlForm";
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      urls: []
-    }
+      urls: [],
+    };
   }
 
   componentDidMount() {
-    getUrls()
-      .then(data => this.setState({urls: data.urls}))
+    getUrls().then((data) => this.setState({ urls: data.urls }));
   }
 
-  updateFormView = (url) =>{
-    this.setState({urls: [...this.state.urls, url]})
-  }
+  updateFormView = (url) => {
+    this.setState({ urls: [...this.state.urls, url] });
+  };
+
+  deleteUrl = async (url) => {
+    let response = await deleteUrl(url);
+    if (response.ok) {
+      let updatedUrls = await getUrls();
+      return this.setState({ urls: updatedUrls.urls });
+    }
+    alert(`No url with ${url.id} found in server`)
+  };
 
   render() {
     return (
       <main className="App">
         <header>
           <h1>URL Shortener</h1>
-          <UrlForm updateFormView={this.updateFormView}/>
+          <UrlForm updateFormView={this.updateFormView} />
         </header>
 
-        <UrlContainer urls={this.state.urls}/>
+        <UrlContainer deleteUrl={this.deleteUrl} urls={this.state.urls} />
       </main>
     );
   }
