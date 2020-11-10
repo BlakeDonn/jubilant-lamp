3f0cdb17be4256808167315e1f52234e21e6664e
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-10 11:30:32 -0500

    Add postUrls fetch

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,15 @@
 export const getUrls = () => {
-  return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
-}
+  return fetch("http://localhost:3001/api/v1/urls").then((response) =>
+    response.json()
+  );
+};
+
+export const postUrls = async () => {
+  const response = await fetch("https://fe-cors-proxy.herokuapp.com", {
+    method: "POST",
+    headers: {
+      "Target-URL": "https://randomfox.ca/floof",
+    },
+  });
+  return await response.json();
+};
