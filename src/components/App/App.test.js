6cfa703bdc4580c19585e52be0ad3517f23eaa25
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-10 13:03:16 -0500

    Add testing for urls gathered on mount

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -8,37 +8,39 @@ jest.mock("../../apiCalls.js");
 
 describe("App", () => {
   beforeEach(() => {
-    getUrls.mockResolvedValue([
-      {
-        id: 1,
-        long_url:
-          "longurl1",
-        short_url: "shorturl1",
-        title: "title1",
-      },
-      {
-       id: 2,
-        long_url:
-          "longurl2",
-        short_url: "shorturl2",
-        title: "title2",
-      },
-      {
-       id: 3,
-        long_url:
-          "longurl3",
-        short_url: "shorturl3",
-        title: "title3",
-      },
-    ]);
+    getUrls.mockResolvedValue({
+      urls: [
+        {
+          id: 1,
+          long_url: "longurl1",
+          short_url: "shorturl1",
+          title: "title1",
+        },
+        {
+          id: 2,
+          long_url: "longurl2",
+          short_url: "shorturl2",
+          title: "title2",
+        },
+        {
+          id: 3,
+          long_url: "longurl3",
+          short_url: "shorturl3",
+          title: "title3",
+        },
+      ],
+    });
   });
   it("Should render with header", () => {
     render(<App />);
     expect(screen.getByText("URL Shortener")).toBeInTheDocument();
   });
 
-  it("Should render any urls stored in server", () => {
+  it("Should render any urls stored in server", async () => {
     render(<App />);
-    screen.debug()
+    await waitFor(() => expect(screen.getByText("title1")).toBeInTheDocument());
+    expect(screen.getByText("title1")).toBeInTheDocument();
+    expect(screen.getByText("title2")).toBeInTheDocument();
+    expect(screen.getByText("title3")).toBeInTheDocument();
   });
 });
