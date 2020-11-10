0e2a17c95b1b8ef6d12f7ced48cf17c7ca5ebbb2
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-11-10 12:02:43 -0500

    Fix inconsistencies in testUrls data import react

--- src/components/UrlContainer/UrlContainer.test.js
+++ src/components/UrlContainer/UrlContainer.test.js
@@ -1,27 +1,30 @@
 import { render, screen, waitFor } from "@testing-library/react";
+import React from 'react'
 import "@testing-library/jest-dom";
 import { UrlContainer } from "./UrlContainer";
 
 
 beforeEach(() => { 
-  testUrls = {
+  const testUrls = [{
     id: 1,
     long_url: "https://longUrl",
     short_url: "https://shortUrl",
     title: "testTitle"
-  }
-  testUrls = {
+  },
+  {
     id: 2,
     long_url: "https://longUrl2",
     short_url: "https://shortUrl2",
     title: "testTitle2"
-  }
-  testUrls = {
+  },
+  {
     id: 3,
     long_url: "https://longUrl3",
     short_url: "https://shortUrl3",
     title: "testTitle3"
-  }
+  }]
+});
+
 describe("UrlContainer", () =>{
   it ("Should render headings of urls", () =>{
     render(<UrlContainer />)
